8e81418b9911802802f8aeda4611c3e1324aed47
Nurseyit Abdyldaev <nurseyit2005@gmail.com>
2024-12-09 12:31:13 +0600

    add size images

--- src/features/scan-media/scan-media.tsx
+++ src/features/scan-media/scan-media.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useRef, useState } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 import FacingCameraIcon from '@/assets/facingCamera.svg';
@@ -7,7 +7,7 @@ import { cropImage } from './lib';
 import { Props } from './model';
 import s from './styles.module.scss';
 
-const HEIGHT_IMAGE = 400;
+const HEIGHT = 400;
 const SCREEN_QUALITY = 1;
 const FRONT_CAMERA = 'user';
 const BACK_CAMERA = { exact: 'environment' };
@@ -16,8 +16,14 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError }) => {
   const [isFrontCamera, setIsFrontCamera] = useState(
     process.env.NODE_ENV !== 'production' ? FRONT_CAMERA : BACK_CAMERA,
   );
+  const [images, setImages] = useState('');
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
 
   const webcamRef = useRef<Webcam>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
 
   const videoConstraints = {
     width: {
@@ -43,30 +49,59 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError }) => {
 
     try {
       const croppedImage = await cropImage(imageSrc);
+      setImages(croppedImage);
       onMakeShot(croppedImage);
     } catch {
       onError();
     }
   }, [webcamRef, onMakeShot, onError]);
 
+  useEffect(() => {
+    if (imgRef.current) {
+      const handleLoad = () => {
+        setImageDimensions({
+          width: imgRef.current!.naturalWidth,
+          height: imgRef.current!.naturalHeight,
+        });
+      };
+
+      imgRef.current.addEventListener('load', handleLoad);
+      return () => {
+        imgRef.current?.removeEventListener('load', handleLoad);
+      };
+    }
+
+    alert(imageDimensions);
+  }, [images, imageDimensions]);
+
   return (
     <div className={s.container}>
       <div className={s.wrapperCamera}>
-        <Webcam
-          style={{
-            width: '100%',
-            height: 'auto',
-          }}
-          audio={false}
-          ref={webcamRef}
-          height={HEIGHT_IMAGE}
-          forceScreenshotSourceSize={true}
-          screenshotQuality={SCREEN_QUALITY}
-          videoConstraints={videoConstraints}
-          onUserMediaError={onError}
-          onUserMedia={() => {}}
-          screenshotFormat="image/jpeg"
-        />
+        {images ? (
+          <>
+            <img ref={imgRef} src={images} alt="Captured" />
+            <p>
+              Image dimensions: {imageDimensions.width}px ×{' '}
+              {imageDimensions.height}px
+            </p>
+          </>
+        ) : (
+          <Webcam
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            audio={false}
+            ref={webcamRef}
+            height={HEIGHT}
+            forceScreenshotSourceSize={true}
+            screenshotQuality={SCREEN_QUALITY}
+            videoConstraints={videoConstraints}
+            onUserMediaError={onError}
+            onUserMedia={() => {}}
+            screenshotFormat="image/jpeg"
+          />
+        )}
       </div>
 
       <div className={s.wrapperButton}>
