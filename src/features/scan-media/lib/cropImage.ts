90a09ede61b0ed6b13c712861083e83ba1048a7a
Nurseyit Abdyldaev <nurseyit2005@gmail.com>
2024-12-16 17:02:44 +0600

    feat: add gallery

--- src/features/scan-media/lib/cropImage.ts
+++ src/features/scan-media/lib/cropImage.ts
@@ -10,47 +10,28 @@ const percentToNumber = (fullNumber: number, percent: number) => {
 
 export const prepareCameraSetting = (passport: boolean, height: number) => {
   let HEIGHT_IMAGE = 230;
+  let TOP_CONTAINER = 15;
   const WIDTH_IMAGE = window.innerWidth;
   const WINDOW_HEIGHT = height;
 
   function getRange(width: number, passport: boolean) {
-    if (width >= 320 && width < 375) return 'range1';
-    if (passport && width >= 375 && width < 393) return 'range2';
-    if (!passport && width >= 393 && width < 480) return 'range9';
-    if (!passport && width >= 480 && width < 550) return 'range5';
-    if (!passport && width >= 550 && width < 600) return 'range6';
-    if (!passport && width >= 600 && width < 650) return 'range7';
-    if (!passport && width >= 650 && width < 700) return 'range8';
+    if (!passport && width >= 320 && width < 425) return 'topingForeign';
+    if (width >= 320 && width < 375) return 'smallContainer';
     return 'default';
   }
 
   switch (getRange(WIDTH_IMAGE, passport)) {
-    case 'range1':
+    case 'smallContainer':
       HEIGHT_IMAGE = 180;
       break;
-    case 'range2':
-      HEIGHT_IMAGE = 200;
-      break;
-    case 'range5':
-      HEIGHT_IMAGE = 300;
-      break;
-    case 'range6':
-      HEIGHT_IMAGE = 350;
-      break;
-    case 'range7':
-      HEIGHT_IMAGE = 370;
-      break;
-    case 'range8':
-      HEIGHT_IMAGE = 410;
-      break;
-    case 'range9':
-      HEIGHT_IMAGE = 400;
+    case 'topingForeign':
+      TOP_CONTAINER = 10;
       break;
     default:
-      console.log('No matching range found');
   }
 
-  const CROP_CENTER_Y = WINDOW_HEIGHT - HEIGHT_IMAGE;
+  const CROP_CENTER_Y = (WINDOW_HEIGHT - HEIGHT_IMAGE) / 2;
+  const CONTAINER_FOREIGN = WINDOW_HEIGHT * 0.9;
 
   return {
     canvasSettings: {
@@ -59,13 +40,13 @@ export const prepareCameraSetting = (passport: boolean, height: number) => {
     },
     cropSettings: {
       sx: percentToNumber(WIDTH_IMAGE, START_CROP_IMAGE_IN_PERCENTAGE_BY_X), // начальная точка вырезки фото по оси X
-      sy: CROP_CENTER_Y / 2, // начальная точка вырезки фото по оси Y
+      sy: passport ? CROP_CENTER_Y : TOP_CONTAINER, // начальная точка вырезки фото по оси Y
       sw: percentToNumber(WIDTH_IMAGE, WIDTH_IMAGE_IN_PERCENTAGE), // ширина фотографии для вырезки
-      sh: HEIGHT_IMAGE, // высота фотографии для вырезки
+      sh: passport ? HEIGHT_IMAGE : CONTAINER_FOREIGN, // высота фотографии для вырезки
       dx: 0, // отступ результата вырезанной фото по оси X
       dy: 0, // отступ результата вырезанной фото по оси Y
       dw: percentToNumber(WIDTH_IMAGE, WIDTH_IMAGE_IN_PERCENTAGE), // ширина вырезанной фотографии
-      dh: HEIGHT_IMAGE, // высота вырезанной фотографии
+      dh: passport ? HEIGHT_IMAGE : CONTAINER_FOREIGN, // высота вырезанной фотографии
     },
   };
 };
--- src/features/scan-media/scan-media.tsx
+++ src/features/scan-media/scan-media.tsx
@@ -2,7 +2,9 @@ import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 import TorchIcon from '@/assets/Torch.svg';
-import FacingCameraIcon from '@/assets/facingCamera.svg';
+import Gallery from '@/assets/gallery.jpeg';
+
+import { clsx } from 'clsx';
 
 import { cropImage, prepareCameraSetting } from './lib';
 import { Props } from './model';
@@ -10,18 +12,14 @@ import s from './styles.module.scss';
 import { cropSettings } from './type';
 
 const SCREEN_QUALITY = 0.5;
-const FRONT_CAMERA = 'user';
-const BACK_CAMERA = { exact: 'environment' };
 
 export const ScanMedia: FC<Props> = ({ onMakeShot, onError, passport }) => {
-  const [isFrontCamera, setIsFrontCamera] = useState(
-    process.env.NODE_ENV !== 'production' ? FRONT_CAMERA : BACK_CAMERA,
-  );
   const [images, setImages] = useState('');
   const [isTorchOn, setIsTorchOn] = useState<boolean>(false);
   const [cropSettings, setCropSettings] = useState<cropSettings | null>(null);
   const [isWebcamReady, setIsWebcamReady] = useState(false);
   const [height, setHeight] = useState(0);
+  const [galleryPhoto, setGalleryPhoto] = useState(false);
   const webcamRef = useRef<Webcam>(null);
   const parentWebcamRef = useRef<HTMLDivElement | null>(null);
 
@@ -31,9 +29,9 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError, passport }) => {
   }, [passport, height]);
 
   const videoConstraints = {
-    width: { min: 320, ideal: 1080, max: 1080 },
-    height: { min: 240, ideal: 1080, max: 1080 },
-    facingMode: isFrontCamera,
+    width: { min: 640, ideal: 1080, max: 1080 },
+    height: { min: 480, ideal: 1080, max: 1080 },
+    facingMode: 'user',
   };
 
   const handleImages = () => {
@@ -66,6 +64,69 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError, passport }) => {
       onError();
     }
   }, [webcamRef, onError, onMakeShot, passport, height]);
+
+  const handleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const compressImage = (image: HTMLImageElement, quality = 0.5) => {
+      return new Promise<string>((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        if (!ctx) {
+          reject(new Error('Canvas context not available'));
+          return;
+        }
+
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        try {
+          const dataUrl = canvas.toDataURL(file.type, quality);
+          resolve(dataUrl);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    };
+
+    const reader = new FileReader();
+    reader.onload = async () => {
+      const imageSrc = reader.result as string;
+      const image = new Image();
+
+      image.onload = async () => {
+        try {
+          const compressedImage = await compressImage(image, 0.5);
+          setGalleryPhoto(true);
+          setImages(compressedImage);
+          onMakeShot(compressedImage);
+        } catch (error) {
+          console.error('Ошибка при сжатии изображения:', error);
+          onError();
+        }
+      };
+
+      image.onerror = () => {
+        console.error('Ошибка загрузки изображения');
+        onError();
+      };
+
+      image.src = imageSrc;
+    };
+
+    reader.onerror = () => {
+      console.error('Ошибка чтения файла');
+      onError();
+    };
+
+    reader.readAsDataURL(file);
+  };
 
   const toggleTorch = () => {
     setIsTorchOn((prev) => !prev);
@@ -101,7 +162,14 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError, passport }) => {
         {passport && !images && isWebcamReady && (
           <div className={s.header}>Отсканируйте обратную сторону паспорта</div>
         )}
-        {images && <img className={s.img} src={images} alt="Captured" />}
+        {images && (
+          <img
+            className={clsx(galleryPhoto && s.galleryPhoto)}
+            src={images}
+            width={window.screen.availWidth}
+            alt="Captured"
+          />
+        )}
         <div ref={parentWebcamRef}>
           <Webcam
             style={{
@@ -138,17 +206,15 @@ export const ScanMedia: FC<Props> = ({ onMakeShot, onError, passport }) => {
         </div>
       ) : (
         <div className={s.wrapperButton}>
+          <div className={s.galleryInput}>
+            <img className={s.img} src={Gallery} alt="gallery" />
+            <input
+              type="file"
+              accept="image/png, image/webp, image/jpeg"
+              onChange={handleFileUpload}
+            />
+          </div>
           <button className={s.button} onClick={captureScreenshot} />
-          <button
-            onClick={() =>
-              setIsFrontCamera((prev) =>
-                prev === FRONT_CAMERA ? BACK_CAMERA : FRONT_CAMERA,
-              )
-            }
-            className={s.btnFrontCamera}
-          >
-            <img src={FacingCameraIcon} alt="Switch Camera" />
-          </button>
           <button className={s.torch} onClick={toggleTorch}>
             <img src={TorchIcon} alt="torch" />
           </button>
