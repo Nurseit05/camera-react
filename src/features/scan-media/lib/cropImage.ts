65c7c0f72c488e9237c6ef713ce5ecfa457b93de
Nurseyit Abdyldaev <nurseyit2005@gmail.com>
2024-12-16 12:46:20 +0600

    fix: top container to international password

--- src/features/scan-media/lib/cropImage.ts
+++ src/features/scan-media/lib/cropImage.ts
@@ -9,15 +9,13 @@ const percentToNumber = (fullNumber: number, percent: number) => {
 };
 
 export const prepareCameraSetting = (passport: boolean) => {
-  let START_CROP_Y = 20;
   let HEIGHT_IMAGE = 230;
   const WIDTH_IMAGE = window.innerWidth;
+  const WINDOW_HEIGHT = window.innerHeight;
 
   function getRange(width: number, passport: boolean) {
     if (width >= 320 && width < 375) return 'range1';
     if (passport && width >= 375 && width < 393) return 'range2';
-    if (passport && width >= 393 && width < 480) return 'range3';
-    if (passport && width >= 480 && width < 768) return 'range4';
     if (!passport && width >= 393 && width < 480) return 'range9';
     if (!passport && width >= 480 && width < 550) return 'range5';
     if (!passport && width >= 550 && width < 600) return 'range6';
@@ -33,26 +31,16 @@ export const prepareCameraSetting = (passport: boolean) => {
     case 'range2':
       HEIGHT_IMAGE = 200;
       break;
-    case 'range3':
-      START_CROP_Y = 90;
-      break;
-    case 'range4':
-      START_CROP_Y = 60;
-      break;
     case 'range5':
-      START_CROP_Y = 15;
       HEIGHT_IMAGE = 300;
       break;
     case 'range6':
-      START_CROP_Y = 15;
       HEIGHT_IMAGE = 350;
       break;
     case 'range7':
-      START_CROP_Y = 20;
       HEIGHT_IMAGE = 370;
       break;
     case 'range8':
-      START_CROP_Y = 20;
       HEIGHT_IMAGE = 410;
       break;
     case 'range9':
@@ -65,6 +53,8 @@ export const prepareCameraSetting = (passport: boolean) => {
   if (passport) alert(WIDTH_IMAGE);
   if (!passport) alert(WIDTH_IMAGE);
 
+  const CROP_CENTER_Y = (WINDOW_HEIGHT - HEIGHT_IMAGE) / 2;
+
   return {
     canvasSettings: {
       height: HEIGHT_IMAGE,
@@ -72,7 +62,7 @@ export const prepareCameraSetting = (passport: boolean) => {
     },
     cropSettings: {
       sx: percentToNumber(WIDTH_IMAGE, START_CROP_IMAGE_IN_PERCENTAGE_BY_X), // начальная точка вырезки фото по оси X
-      sy: START_CROP_Y, // начальная точка вырезки фото по оси Y
+      sy: CROP_CENTER_Y, // начальная точка вырезки фото по оси Y
       sw: percentToNumber(WIDTH_IMAGE, WIDTH_IMAGE_IN_PERCENTAGE), // ширина фотографии для вырезки
       sh: HEIGHT_IMAGE, // высота фотографии для вырезки
       dx: 0, // отступ результата вырезанной фото по оси X
